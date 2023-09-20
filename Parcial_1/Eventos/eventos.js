e17cebe3eda82c6a4636c38566137a4e3a0a34e1
Barrabasito <juan72764@hotmail.com>
2023-09-20 08:40:41 -0500

    Terminado y revisado

---
+++ Parcial_1/Eventos/eventos.js
@@ -0,0 +1,17 @@
+const events = require('events');
+const emisor = new events.EventEmitter();
+
+function saludar() {
+    const emisor = new events.EventEmitter();
+    setTimeout(()=>emisor.emit('saluda','Juan'), 3000);
+    setTimeout(()=>emisor.emit('saluda','Oscar'), 4000);
+    setTimeout(()=>emisor.emit('saluda','Luz'), 5000);
+    emisor.emit('saluda', 'Juan');
+    return emisor;
+}  
+
+let sal = saludar();
+
+sal.on('saluda', (nombre) =>{
+    console.log('Hola ' +nombre);
+});
